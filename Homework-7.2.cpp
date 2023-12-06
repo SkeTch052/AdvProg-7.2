0513bc393cc08c16fa0a8099daa53a1dd37f942b
SkeTch <sketch13rus@mail.ru>
2023-12-06 16:45:03 +0300

    Initial commit

---
+++ Homework-7.2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+#include <string>
+
+template <typename Container>
+void PrintContainer(const Container& c) {
+    for (const auto& elem : c) {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::set<std::string> set = { "one", "two", "three", "four" };
+    std::vector<std::string> vector = { "one", "two", "three", "four" };
+    std::list<std::string> list = { "one", "two", "three", "four" };
+
+    std::cout << "Set: "; PrintContainer(set);
+    std::cout << "Vector: "; PrintContainer(vector);
+    std::cout << "List: "; PrintContainer(list);
+
+    return 0;
+}
